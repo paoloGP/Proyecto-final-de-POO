9500573f2dcad8b5a4f5f09b3aa8fb679e15c984
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-06-26 12:58:23 -0500

    Cambios hechos en clase

--- Tierra.cpp
+++ Tierra.cpp
@@ -1,24 +1,23 @@
-//
-// Created by utec on 21/06/19.
-//
-
 #include "Tierra.h"
 #include <string>
 #include <iomanip>
 #include <algorithm>
 
+map<char,sf::Color> colores = {{'R',sf::Color::Red},{'G',sf::Color::Green},{'B',sf::Color::Blue}};
 using namespace std;
 
 Tierra::Tierra() {
-    plano.resize(ALTURA);
-    for (auto& item: plano)
-        item.resize(ANCHO);
+    plano = nullptr;
+//    plano.resize(ALTURA);
+//    for (auto& item: plano)
+//        item.resize(ANCHO);
 }
 
 Tierra::Tierra(TipoEntero altura, TipoEntero ancho) {
-    plano.resize(altura);
-    for (auto& item: plano)
-        item.resize(ancho);
+    plano = new sf::RenderWindow(sf::VideoMode(ancho,altura),"Proyecto Final");
+//    plano.resize(altura);
+//    for (auto& item: plano)
+//        item.resize(ancho);
 }
 
 Tierra::~Tierra() {}
@@ -54,35 +53,42 @@ void Tierra::imprimirObjetos() {
 }
 
 void Tierra::actualizarTierra() {
-    for (auto &row: plano)
-        for (auto &cell: row)
-            cell = COLOR;
-
-    for (auto& item: objetos)
-        plano[item->getPosX()][item->getPosY()]
-                = item->getColor();
+    for(auto& item : objetos) {
+        sf::CircleShape shape(10);
+        shape.setFillColor(colores[item->getColor()]);
+        shape.setPosition(item->getPosX(), item->getPosY());
+        plano->draw(shape);
+    }
+//    for (auto &row : plano)
+//        for (auto &cell: row)
+//            cell = COLOR;
+//
+//    for (auto& item: objetos)
+//        plano[item->getPosX()][item->getPosY()]
+//                = item->getColor();
 }
 
 void Tierra::dibujarTierra() {
-    cout << '\n';
-    cout << setw(3) << ' ';
-    for (auto j = 0; j < getAncho(); ++j)
-        cout << setw(3) << j;
-    cout << '\n';
-
-    for (auto i = 0; i < getAltura(); ++i) {
-        cout << setw(3) << i;
-        for (auto j = 0; j < getAncho(); ++j) {
-            cout << setw(3) << plano[i][j];
+    if(plano == nullptr) return;
+    while (plano->isOpen())
+    {
+        sf::Event event;
+        while (plano->pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                plano->close();
         }
-        cout << '\n';
+
+        plano->clear();
+        actualizarTierra();
+        plano->display();
     }
 }
 
 TipoEntero Tierra::getAltura() {
-    return plano.size();
+    return plano->getPosition().y;
 }
 
 TipoEntero Tierra::getAncho(){
-    return plano[0].size();
+    return plano->getPosition().x;
 }
