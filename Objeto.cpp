49f39d51770dc7ff05ac4b8ee7f555b929bc71f0
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-06-29 13:16:19 -0500

    cambios
    Se agrego al los dos constructores el atributo "figura" y también se agregó el código de getFigura.

--- Objeto.cpp
+++ Objeto.cpp
@@ -5,12 +5,12 @@
 
 #include "Objeto.h"
 
-Objeto::Objeto(): color{}, posX{}, posY{}  {}
+Objeto::Objeto(): color{}, posX{}, posY{}, figura{}  {}
 
 Objeto::Objeto(const TipoString& nombre, TipoCaracter color,
-               TipoEntero posX, TipoEntero posY):
+               TipoEntero posX, TipoEntero posY, TipoEntero fig):
         nombre{nombre}, color{color},
-        posX{posX}, posY{posY} {}
+        posX{posX}, posY{posY}, figura{fig} {}
 
 Objeto::~Objeto() {}
 
@@ -21,7 +21,9 @@ TipoString   Objeto::getNombre() { return nombre; }
 TipoEntero   Objeto::getPosX()   { return posX; }
 TipoEntero   Objeto::getPosY()   { return posY; }
 TipoCaracter Objeto::getColor()  { return color; }
+TipoEntero   Objeto::getFigura() { return figura;}
 
 TipoString Objeto::mostrarPosicion() {
     return "X = " + to_string(posX) + " Y = " + to_string(posY);
 }
+
