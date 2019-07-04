73b6a133230e66775b5c6a9090fae2800e6b5857
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-07-03 21:35:47 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -4,7 +4,13 @@
 
 
 int main(){
-    Menu menu;
+    TipoEntero altura , ancho;
+    cout << "Ingresar altura de ventana: ";
+    cin >> altura;
+    cout << endl;
+    cout << "Ingresar ancho de ventana: ";
+    cin >> ancho;
+    Menu menu(ancho,altura);
     menu.ejecutar();
     return 0;
 }
@@ -13,20 +19,18 @@ int main(){
 int main(){
 
     sf::RenderWindow window(sf::VideoMode(600, 600), "SFML works!");
-    while (window.isOpen()){
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            window.clear();
-            if(sf::Mouse::isButtonPressed(sf::Mouse::Left) && event.mouseButton.x < 600 && event.mouseButton.y < 600){
-                cout << window.getPosition().x << "  " << window.getPosition().y << endl;
-                sf::RectangleShape shape(sf::Vector2f(50, 50));
-                shape.setFillColor(sf::Color(24, 65, 3, 255));
-                shape.setPosition(event.mouseButton.x, event.mouseButton.y);
-                window.draw(shape);
+        while (window.pollEvent(event)) {
+            int x =0 , y = 0;
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                x = event.mouseButton.x;
+                y = event.mouseButton.y;
+                cout << x << "  " << y << endl;
+                if((x > 0 && x<600) || (y < 600 && y > 0)) {
+                    window.close();
+                }
             }
-            if (event.type == sf::Event::Closed)
-                window.close();
             window.display();
         }
     }
