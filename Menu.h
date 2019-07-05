408107c3f18b669e9529fb66b3d8d4dc6e1f7e3e
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-07-05 02:39:04 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -5,38 +5,17 @@
 #include "Tierra.h"
 #include "Tipos.h"
 
-template <typename T>
-T input(const string& label) {
-    T value;
-    cout << label;
-    cin >> value;
-    cin.clear();
-    cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-    return value;
-}
-
-/*
-//--- Esta seria la funcion para leer un string,
-//--- esta funcion si se quiere generalizar para colocar un label
-//--- y leer cualquier tipo de dato se tendria que convertir al template anterior
-
-string&& input(string label) {
-  string value;
-  cout << label;
-  cin >> value;
-  return move(value);
-}
- */
-
-
 class Menu {
     TipoEntero opcion;
     Tierra tierra;
-    void mostrarMenu();
+    static void mostrarMenu();
     void seleccionarOpcion();
     void agregarObjeto();
     void removerObjeto();
     void dibujarMapa();
+    void masCercano();
+    void tmejores();
+    void mejores();
 public:
     Menu(TipoEntero _ancho, TipoEntero _altura);
     virtual ~Menu();
