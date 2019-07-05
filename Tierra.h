ffd6b16db1cc4649ab5c46ddcf940327507051c3
paoloGP <39799467+paoloGP@users.noreply.github.com>
2019-07-05 02:32:51 -0500

    Update Tierra.h

--- Tierra.h
+++ Tierra.h
@@ -2,11 +2,26 @@
 #define GAME_TIERRA_H
 
 #include <iostream>
+#include <math.h>
 #include <SFML/Graphics.hpp>
 #include <limits>
 #include <vector>
 #include "Tipos.h"
-#include "Objeto.h"
+#include "Museo.h"
+#include "Restaurante.h"
+#include "Hotel.h"
+
+
+template <typename T>
+T input(const string& label) {
+    T value;
+    cout << label;
+    cin >> value;
+    cin.clear();
+    cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+    return value;
+}
+
 using namespace std;
 
 class Tierra {
@@ -14,17 +29,22 @@ private:
     TipoEntero altura;
     TipoEntero ancho;
     sf::RenderWindow* plano;
-    vector<Objeto*> objetos;
+    vector<Hotel*> hoteles;
+    vector<Restaurante*> restaurantes;
+    vector<Museo*> museos;
 public:
     Tierra();
-    Tierra(TipoEntero _ancho, TipoEntero _altura);
+    Tierra(TipoEntero& _ancho, TipoEntero& _altura);
     virtual ~Tierra();
-    void adicionarObjeto();
+    void adicionarObjeto(TipoString& _nombre, TipoCaracter& _color, TipoCaracter& _lugar,  TipoEntero& _cali);
     Objeto* removerObjeto(string& nombre);
     void imprimirObjetos();
-    TipoEntero getAltura();
-    TipoEntero getAncho();
-    TipoEntero getCantidadObjectos();
+    TipoEntero getCantidadHoteles();
+    TipoEntero getCantidadMuseos();
+    TipoEntero getCantidadRestaurantes();
+    void masCercano();
+    void tmejores();
+    void mejores();
     void actualizarTierra();
     void capturarEventos();
     void dibujarTierra();
